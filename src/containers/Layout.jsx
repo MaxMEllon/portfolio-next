0c861e5c38a55d0e4f99560ee1091e0dd788c15f
MaxMEllon <maxmellon1994@gmail.com>
2017-05-14 08:19:55 +0900

    heart

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import RouteCSSTransitionGroup from './RouteCSSTransitionGroup';
 import Navbar from '../components/Navbar';
 import Loading from '../nodes/Loading';
+import SkipStone from '../nodes/SkipStone';
 
 export default function Layout({ children }) {
   return (
     <div>
       <Navbar title="maxmellon.github.io" />
       <Loading />
+      <SkipStone />
       <RouteCSSTransitionGroup
         component="div" transitionName="routing"
         transitionEnterTimeout={1000} transitionLeaveTimeout={400}
